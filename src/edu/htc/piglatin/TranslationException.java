7e79419e5ed5d339a8f99e8b94ccd0d7f212b619
radhikaprabu <radhikaprabu91699@gmail.com>
2016-04-10 23:21:38 -0500

    Project 2

---
+++ src/edu/htc/piglatin/TranslationException.java
@@ -0,0 +1,10 @@
+package edu.htc.piglatin;
+
+/**
+ * Created by Owner on 4/10/2016.
+ */
+public class TranslationException extends Exception {
+    public TranslationException(String message){
+        super(message);
+    }
+}
--- test/edu/htc/piglatin/PigLatinTranslatorTest.java
+++ test/edu/htc/piglatin/PigLatinTranslatorTest.java
@@ -1,7 +1,6 @@
 package edu.htc.piglatin;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,77 +10,134 @@ public class PigLatinTranslatorTest {
 
     @Test
     public void translateToPigLatin(){
-
+    String englishText = "Hello Translator.";
+        Assert.assertEquals(PigLatinTranslator.translateToPigLatin(englishText),"Ello\'hay Anslator\'tray.");
     }
 
     @Test
     public void translateToPigLatin_emptyAndBlankStrings(){
+        String englishText = " ";
+        Assert.assertEquals(PigLatinTranslator.translateToPigLatin(englishText)," ");
 
     }
 
+
     @Test
     public void translateFromPigLatin(){
+        String pigLatinText = "Ello'hay anslator'tray.";
+        try {
+            Assert.assertEquals(PigLatinTranslator.translateFromPigLatin(pigLatinText), "Hello translator.");
+        }catch (TranslationException te){
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void translateFromPigLatin_emptyAndBlankStrings(){
+        String pigLatinText = " ";
+        try {
+            Assert.assertEquals(PigLatinTranslator.translateFromPigLatin(pigLatinText), " ");
+        }catch (TranslationException te){
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void wordToPigLatin_emptyAndBlankStrings() {
+        String englishText = " ";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText), " ");
 
     }
 
     @Test
     public void wordToPigLatin_startsWithLowercaseVowel() {
+        String englishText = "element";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText), "element\'ay");
 
     }
 
     @Test
     public void wordToPigLatin_withPunctuation() {
+        String englishText = "objects,";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText), "objects\'ay,");
 
     }
 
     @Test
     public void wordToPigLatin_startsWithCapitalVowel() {
+        String englishText = "Apples";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText), "Apples\'ay");
 
     }
 
     @Test
     public void wordToPigLatin_startsWithLowercaseConsonant() {
+        String englishText = "guide";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText),"uide\'gay");
 
     }
 
     @Test
     public void wordToPigLatin_startsWithCapitalConsonant() {
+        String englishText = "Sixth";
+        Assert.assertEquals(PigLatinTranslator.wordToPigLatin(englishText), "Ixth\'say");
 
     }
 
     @Test
     public void wordFromPigLatin_emptyAndBlankStrings() {
+        String pigLatinText = " ";
+        try {
+            Assert.assertEquals(PigLatinTranslator.wordFromPigLatin(pigLatinText), " ");
+        } catch (TranslationException te) {
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void wordFromPigLatin_startsWithLowercaseVowel() {
+        String pigLatinText = "uide\'gay";
+        try {
+            Assert.assertEquals(PigLatinTranslator.wordFromPigLatin(pigLatinText), "guide");
+        } catch (TranslationException te) {
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void wordFromPigLatin_startsWithCapitalVowel() {
+        String pigLatinText = "Apples\'ay";
+        try {
+            Assert.assertEquals(PigLatinTranslator.wordFromPigLatin(pigLatinText), "Apples");
+        } catch (TranslationException te) {
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void wordFromPigLatin_startsWithLowercaseConsonant() {
+        String pigLatinText = "ger\'ginay";
+        try {
+            Assert.assertEquals(PigLatinTranslator.wordFromPigLatin(pigLatinText), "ginger");
+        } catch (TranslationException te) {
+            System.out.println(te.getMessage());
+        }
 
     }
 
     @Test
     public void wordFromPigLatin_startsWithCapitalConsonant() {
-
+        String pigLatinText = "Ger\'ginay";
+        try {
+            Assert.assertEquals(PigLatinTranslator.wordFromPigLatin(pigLatinText), "Ginger");
+        } catch (TranslationException te) {
+            System.out.println(te.getMessage());
+        }
     }
 
 }
